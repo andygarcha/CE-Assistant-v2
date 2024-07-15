683c26682bf64db924695f593b806888ea3856d4
andy :) <89205919+andykasen13@users.noreply.github.com>
2024-07-15 14:09:17 -0400

    Add error reporting

--- Classes/CE_User.py
+++ Classes/CE_User.py
@@ -447,6 +447,7 @@ class CEAPIUser(CEUser) :
         
         # pull the data
         objective_tuples = self.most_recent_objectives()
+        if objective_tuples is None : return "Error. Please try again or ping andy."
 
         # set up return
         return_str : str = ""
