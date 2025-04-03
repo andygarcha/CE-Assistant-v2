b7edcb9782069bb6278746c9240fd23c5ca2de4d
andy :) <agarcha9@gatech.edu>
2025-04-03 15:17:06 -0400

    Update CE_Roll.py

--- Classes/CE_Roll.py
+++ Classes/CE_Roll.py
@@ -528,7 +528,7 @@ class CERoll:
                 game_name = hm.get_item_from_list(game, database_name)
             return(
                 f"Sorry <@{user.discord_id}>, you failed your {self.roll_name} roll ({game_name.game_name})." +
-                f"You are now on cooldown for {self.roll_name} until <t:{self.calculate_cooldown_date(database_name)}>."
+                f"You are now on cooldown for {self.roll_name} until <t:{self.calculate_cooldown_date(database_name)}>.")
         else :
             return (
                 f"Sorry <@{user.discord_id}>, you failed your {self.roll_name} roll. " +
