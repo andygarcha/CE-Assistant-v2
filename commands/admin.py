95d6c91f1057b4ffcaf235df3e09046ed4406646
andy :) <agarcha9@gatech.edu>
2025-10-10 12:54:26 -0400

    Update admin.py

--- commands/admin.py
+++ commands/admin.py
@@ -277,8 +277,6 @@ async def clear_roll_portion(interaction: discord.Interaction, member: discord.M
     roll.set_status('waiting')
     roll.due_time = None
 
-    user.update_current_roll(roll)
-
     await Mongo_Reader.dump_user(user)
     return await interaction.followup.send(f"Removed {game_removed} from {user.display_name}'s {roll_name} roll." +
                                            f"Status set to 'waiting'.")
