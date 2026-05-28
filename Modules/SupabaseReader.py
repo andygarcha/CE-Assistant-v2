13394ad90b38c2849e07ddc68d518fae6ebd75eb
andy :) <agarcha9@gatech.edu>
2026-05-28 08:37:39 -0400

    commit json serializable issue

--- Modules/SupabaseReader.py
+++ Modules/SupabaseReader.py
@@ -561,7 +561,7 @@ def bulk_dump_games(
                     "category_primary": None,
                     "image_header": game._banner,
                     "image_icon": "",
-                    "updated_at_CE": datetime.datetime.now(),
+                    "updated_at_CE": datetime.datetime.now(datetime.timezone.utc).isoformat(),
                 }
             )
 
