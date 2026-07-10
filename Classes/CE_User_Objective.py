2719ee6fa0763692cdfcf6b804f8cb9ecd09d8f7
andy :) <andydotmp3@gmail.com>
2026-07-10 15:41:54 -0400

    feat: add .type_short to CEObjective

--- Classes/CE_User_Objective.py
+++ Classes/CE_User_Objective.py
@@ -44,6 +44,11 @@ class CEUserObjective:
     def type(self) -> hm.OBJECTIVE_TYPES:
         """Returns the type of this Objective (e.g. Community, Primary)."""
         return self._type
+    
+    @property
+    def type_short(self) -> str:
+        """Returns this as a short type (e.g. PO, SO)"""
+        return f"{self._type[0]}O"
 
     @property
     def game_ce_id(self) -> str:
